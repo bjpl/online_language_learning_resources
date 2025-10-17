78213b4f9a4b1c8cf303c694a9680abea787387d
bjpl <brandon.lambert87@gmail.com>
2025-10-16 18:35:40 -0700

    fix: Make languageData globally accessible in module context
    - Attach languageData to window object for ES6 module compatibility
    - Ensures review tool can access data when scripts load as modules

--- assets/js/data-simple.js
+++ assets/js/data-simple.js
@@ -4,5 +4,7 @@
 
 // Initialize global languageData object
 // Individual language data files will populate this object
+// Make it truly global by attaching to window (works in both regular scripts and modules)
 // eslint-disable-next-line no-unused-vars -- Used by individual language files to populate data
 const languageData = {};
+window.languageData = languageData;
